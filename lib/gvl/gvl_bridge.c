eb0b7a2a4d3e9c3051b15d3844e3990c29ecd5a2
stinney <stinney@upenn.edu>
2023-10-17 23:11:39 -0400

    gvl_bridge replaces more gdl/cuneify functionality

--- lib/gvl/gvl_bridge.c
+++ lib/gvl/gvl_bridge.c
@@ -2,33 +2,33 @@
 #include <pool.h>
 #include <gdl.h>
 
+static Hash **bridge_a = NULL;
 static Hash *bridge_h = NULL;
 static Pool *bridge_p = NULL;
 extern int asl_flex_debug , gdl_flex_debug, nwarning;
+static gvl_g *gbgp;
 
-#if 0
-void
-gvl_bridge_qpc(void)
+const char *
+gvl_bridge_atf2utf(void)
 {
-  static int qpc_set = 0;
-  if (!qpc_set)
-    {
-      gvl_i *gip = gvl_setup("pctc", "pctc", "900");
-      langcore_set_sindex("900", gip->sindex);
-      ++qpc_set;
-    }
+  return gbgp->orig;
 }
-#endif
+
+unsigned const char *
+gvl_bridge_cuneify(void)
+{
+  return gvl_cuneify_gv(gbgp);
+}
 
 void
 gvl_bridge_init(void)
 {
-  bridge_h = hash_create(1024);
+  /* This is just enough to work with oxx because it assumes just
+     ogsl/pcsl as possible sl */
+  bridge_a = malloc(3 * sizeof(Hash*));
+  bridge_a[1] = hash_create(1024);
+  bridge_a[2] = hash_create(1024);
   bridge_p = pool_init();
-#if 0
-  gvl_i *gip = gvl_setup("ogsl", "ogsl", "020");
-  langcore_set_sindex("020", gip->sindex);
-#endif
   gdlparse_init();
   mesg_init();
   gdl_flex_debug = 0;
@@ -40,28 +40,25 @@ gvl_bridge(const char *f,size_t l,const unsigned char *g, int sindex)
 {
   Tree *tp = NULL;
   static last_sindex = -1;
-
+  
   if (sindex != last_sindex)
     last_sindex = gvl_switch(sindex);
 
   if (last_sindex == -1)
     return NULL;
 
-#if 0
-  const char *sentinel = "tumₓ(|TUM×GAN₂@t|)";
-  if (!strcmp((ccp)g, sentinel))
-    fprintf(stderr, "gvl_bridge: found %s\n", sentinel);
-#endif
+  if ((gbgp = hash_find(bridge_a[sindex], g)))
+    return gbgp->mess;
   
   tp = gdlparse_string(mesg_mloc(f,l), (char*)pool_copy(g,bridge_p));
 
   if (tp && tp->root && tp->root->kids)
     {
-      gvl_g *gp = tp->root->kids->user;
-      if (gp)
+      gbgp = tp->root->kids->user;
+      if (gbgp)
 	{
-	  hash_add(bridge_h, pool_copy(g,bridge_p), gp);
-	  return (ccp)gp->mess;
+	  hash_add(bridge_a[sindex], pool_copy(g,bridge_p), gbgp);
+	  return (ccp)gbgp->mess;
 	}
       else if (tp->root->kids->kids)
 	{
@@ -70,9 +67,9 @@ gvl_bridge(const char *f,size_t l,const unsigned char *g, int sindex)
 	     root->kids->kids */
 	  if (!strcmp(tp->root->kids->kids->name, "g:n"))
 	    {
-	      gp = tp->root->kids->kids->user;
-	      if (gp)
-		return (ccp)gp->mess;
+	      gbgp = tp->root->kids->kids->user;
+	      if (gbgp)
+		return (ccp)gbgp->mess;
 	    }
 	}
     }
--- lib/gvl/gvl_bridge.h
+++ lib/gvl/gvl_bridge.h
@@ -1,4 +1,6 @@
 #ifndef GVL_BRIDGE_H_
 #define GVL_BRIDGE_H_
+extern const char *gvl_bridge_atf2utf(void);
+extern unsigned const char *gvl_bridge_cuneify(void);
 extern const char *gvl_bridge(const char *f,size_t l,const unsigned char *g, int sindex);
 #endif/*GVL_BRIDGE_H_*/
