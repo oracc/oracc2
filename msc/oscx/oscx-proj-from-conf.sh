dd56f909ddba0f8fa8a90d3004a7b12a0eff4462
Steve Tinney <stinney@upenn.edu>
2023-11-16 15:08:46 +0000

    oscx beginnings

---
+++ msc/oscx/oscx-proj-from-conf.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ -r 00lib/subconfig.xml ]; then
+    fig=00lib/subconfig.xml
+elif [ -r 00lib/config.xml ]; then
+    fig=00lib/config.xml
+else
+    fig=""
+fi
+if [ "$fig" = "" ]; then
+    /bin/echo -n ''
+else
+    /bin/echo -n `perl -pe 's/^<.xml.*>//' $fig | tr '\n' ' ' | perl -ne '/n="(.*?)"/ && print $1'`
+fi
