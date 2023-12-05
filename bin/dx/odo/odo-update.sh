1ed563cf5cb9e265adbc64a8b297af991a0379e4
Steve Tinney <stinney@upenn.edu>
2023-12-05 17:21:33 +0000

    pass args from odo update to mcu*.sh

--- bin/dx/odo/odo-update.sh
+++ bin/dx/odo/odo-update.sh
@@ -4,18 +4,18 @@
 echo $0 $*
 
 # update OIDs
-mcu-oids.sh
+mcu-oids.sh $*
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 # update signlist
-mcu-slix.sh
+mcu-slix.sh $*
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 # update lemm
-mcu-lemm.sh
+mcu-lemm.sh $*
 
 exit $?
