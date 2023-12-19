5bc06098f5b832c42dc57af40677e5c4971aef1f
stinney <stinney@upenn.edu>
2023-12-19 17:54:13 +0000

    strip test funcs out of tok2tis.c

--- bin/xx/tok2tis.c
+++ bin/xx/tok2tis.c
@@ -13,55 +13,6 @@ struct tokloc
 struct tokloc tl;
 Pool *p;
 
-void
-xtis_sort(Tisp tp)
-{
-  const char **k = hash_keys(tp);
-  fprintf(stderr, "tis:sort k[0] = %s\n", k[0]);
-  int i;
-  for (i = 0; k[i]; ++i)
-    {
-      Memo *mp = hash_find(tp, (uccp)k[i]);
-      size_t nk;
-      char **tk = memo_keys(mp, &nk);
-      fprintf(stderr, "tis:sort tk[0] = %s\n", tk[0]);
-      qsort(tk, nk, sizeof(const char *), cmpstringp);
-      memo_term(mp);
-      hash_add(tp, (uccp)k[i], tk);
-    }
-}
-
-void
-xtis_dump(FILE *fp, Tisp tp)
-{
-  int nk;
-  const char **k = hash_keys2(tp, &nk);
-  fprintf(stderr, "before qsort\n");
-  qsort(k,nk,sizeof(const char *), cmpstringp);
-  fprintf(stderr, "after qsort\n");
-  int i;
-  for (i = 0; i < nk; ++i)
-    {
-      fprintf(stderr, "i=%d; k=%s\n", i, k[i]);
-      fputs(k[i], fp);
-      fputc('\t', fp);
-      char **is = hash_find(tp, (uccp)k[i]);
-      if (is)
-	{
-	  int j;
-	  for (j = 0; is[j]; ++j)
-	    {
-	      fprintf(stderr, "j=%d; is[j] = %s\n", j, is[j]);
-	      fputc(' ', fp);
-	      fputs(is[j], fp);
-	    }
-	}
-      else
-	fprintf(stderr, "no is data found for key %s\n", k[i]);
-      fputc('\n', fp);
-    }
-}
-
 int
 main(int argc, char **argv)
 {
