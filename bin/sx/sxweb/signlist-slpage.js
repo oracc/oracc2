9a66e4c5e334b3b9207576793996e16a6af581f7
Steve Tinney <stinney@upenn.edu>
2024-02-23 18:48:14 +0000

    sx jsmap more compound lookups

--- bin/sx/sxweb/signlist-slpage.js
+++ bin/sx/sxweb/signlist-slpage.js
@@ -49,11 +49,18 @@ function slpage () {
     var form=document.getElementById('sl');
     // alert('form='+form);
     var key=document.getElementById('k').value;
+    var orig_key = key;
     // alert('key='+key);
     var ctdr = '';
     if (key.startsWith('*')) {
-	ctdr = '-ctd';
-	key = key.substring(1);
+	if (key.endsWith('*')) {
+	    ctdr = '-cmemb';
+	    key = key.substring(1);
+	    key = key.substring(0, key.length - 1);
+	} else {
+	    ctdr = '-ctd';
+	    key = key.substring(1);
+	}
 	alert('key='+key+'; ctdr='+ctdr);
     } else if (key.endsWith('*')) {
 	ctdr = '-ctr';
@@ -61,8 +68,6 @@ function slpage () {
 	alert('key='+key+'; ctdr='+ctdr);
     }
     var mkey = kmap(key);
-    // alert(mkey);
-    // alert('mkey='+mkey);
     
     var oc = mkey.charCodeAt(0) - "a".charCodeAt(0);
     if (oc >= 0 && oc <= 26) {
@@ -76,7 +81,33 @@ function slpage () {
 	    } else if (ctdr.length > 0) {
 		oid = oid.replace(/\w+/, "");
 		alert('oid-sans-letter='+oid);
-                window.location=projpath+'selpages'+oid+ctdr+'.html';		
+		var oidcheck = oid.substring(1);
+		var ok = 0;
+		var cerr = '';
+		if (ctdr === '-cmemb') {
+		    if (memb.includes(oidcheck)) {
+			ok = 1;
+		    } else {
+			cerr = 'member';
+		    }
+		} else if (ctdr === '-ctr') {
+		    if (ctnr.includes(oidcheck)) {
+			ok = 1;
+		    } else {
+			cerr = 'container';
+		    }
+		} else {
+		    if (ctnd.includes(oidcheck)) {
+			ok = 1;
+		    } else {
+			cerr = 'contained sign';
+		    }
+		}
+		if (ok) {
+                    window.location=projpath+'selpages'+oid+ctdr+'.html';
+		} else {
+		    alert('Lookup key '+orig_key+' has no entry as a compound '+cerr);
+		}
 	    } else {
                 window.location=projpath+oid+'/index.html';
             }
@@ -84,32 +115,9 @@ function slpage () {
             if (key !== mkey) {
                 key += ' (> '+mkey+')';
             }
-            alert('Nothing found for '+key+': if this is a corpus signlist that value may not be known in this corpus');
+            alert('Nothing found for '+orig_key+': if this is a corpus signlist that value may not be known in this corpus');
         }
     } else {
-        alert('Invalid lookup table key "'+key+'": keys must begin with a letter');
-    }
-}
-
-function xslpage () {
-    var form=document.getElementById('sl');
-    // alert('form='+form);
-    var key=document.getElementById('k').value;
-    // alert('key='+key);
-    var mkey = kmap(key);
-    // alert('mkey='+mkey);
-    var oid=x[mkey];
-    if (oid) {
-	// alert('oid='+oid);
-	if (oid.startsWith("selpages/")) {
-	    window.location=projpath+oid+'.html';
-	} else {
-	    window.location=projpath+oid+'/index.html';
-	}
-    } else {
-	if (key !== mkey) {
-	    key += ' (> '+mkey+')';
-	}
-	alert('Nothing found for '+key+': if this is a corpus signlist that value may not be known in this corpus');
+        alert('Invalid lookup table key "'+orig_key+'": keys must begin with a letter');
     }
 }
