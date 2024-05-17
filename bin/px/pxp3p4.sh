f5d19a8903305887e04cfe733399cfbb152a9c21
Steve Tinney <stinney@upenn.edu>
2024-05-17 07:29:48 +0000

    px pxp3p4.sh remove pxlangs.sh

--- bin/px/pxp3p4.sh
+++ bin/px/pxp3p4.sh
@@ -12,7 +12,8 @@ libscripts=${ORACC_BUILDS}/lib/scripts
 xsltproc $libscripts/p3-trans-p4.xsl 02xml/trans-select.xml \
 	 >02xml/p4-trans-select.xml
 chmod o+r 02xml/p4-trans-select.xml
-pxlangs.sh
+# This is called by p3-project-data.plx
+# pxlangs.sh
 pxsortercodes.sh $project
 # p3-project-data.plx doesn't do the following bit
 for a in 02pub/cbd/* ; do
