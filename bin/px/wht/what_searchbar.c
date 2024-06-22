8591be6900232db77f2e7dc62e1fcf7898e2a3e7
Steve Tinney <stinney@upenn.edu>
2024-06-22 23:54:29 +0000

    px implement what_searchbar

--- bin/px/wht/what_searchbar.c
+++ bin/px/wht/what_searchbar.c
@@ -1,9 +1,14 @@
 #include <oraccsys.h>
-#include "../px.h"
+#include "../pxdefs.h"
+#include "../pui/pui.h"
 #include "what.h"
 
 int
 what_searchbar(Isp *ip)
 {
-  return 0;
+  ip->from = "sbar";
+  if (pui_output(ip, stdout, pui_filetext("p4searchbar.xml")))
+    return PX_ERROR;
+  else
+    return PX_DONE;
 }
--- bin/px/wht/whatfuncs.g
+++ bin/px/wht/whatfuncs.g
@@ -4,5 +4,7 @@
 %}
 struct whatfuncstab;
 %%
+score, &what_score
+searchbar, &what_searchbar
 sig, &what_sig
 statistics, &what_statistics
