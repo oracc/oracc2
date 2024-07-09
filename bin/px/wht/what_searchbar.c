aca99b89760d149c972b5de365d3729a6a1b2621
Steve Tinney <stinney@upenn.edu>
2024-07-09 20:30:19 +0000

    px searchbar print hdr

--- bin/px/wht/what_searchbar.c
+++ bin/px/wht/what_searchbar.c
@@ -1,12 +1,14 @@
 #include <oraccsys.h>
 #include "../pxdefs.h"
 #include "../pui/pui.h"
+#include "../wpx_lib.h"
 #include "what.h"
 
 int
 what_searchbar(Isp *ip)
 {
   ip->from = "sbar";
+  wpx_print_hdr();
   if (pui_output(ip, stdout, pui_filetext("p4searchbar.xml")))
     return PX_ERROR;
   else
