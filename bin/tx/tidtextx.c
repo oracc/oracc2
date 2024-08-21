39b5d15c3a3d3d96ed3d421362ae7b1ea86e1436
Steve Tinney <stinney@upenn.edu>
2024-08-21 15:14:59 +0000

    mx nx vacuous initialization

---
+++ bin/tx/tidtextx.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** prepend id_text to CDLI catalog output which has artifact_id in
+ *  col1
+ */
+
+int
+main(int argc, char **argv)
+{
+  int ch;
+  fputs("id_text\t", stdout);
+  while (EOF != (ch=getchar()))
+    {
+      putchar(ch);
+      if ('\n' == ch)
+	break;
+    }
+  int lnum = 2;
+  while (!feof(stdin))
+    {
+      int iid;
+      int ret = scanf("%i",&iid);
+      if (ret)
+	{
+	  printf("P%06d\t%d\t", iid, iid);
+	  while (EOF != (ch=getchar()))
+	    {
+	      putchar(ch);
+	      if ('\n' == ch)
+		{
+		  ++lnum;
+		  break;
+		}
+	    }
+	}
+      else
+	{
+	  fprintf(stderr, "tidtextx: bad artifact_id at line %d\n", lnum);
+	}
+    }
+}
