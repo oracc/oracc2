4bf33e038096d663b4cb022a4eb5ce72574760a8
Steve Tinney <stinney@upenn.edu>
2024-09-04 03:07:55 +0000

    tokx emits basic 'l' data

--- lib/tok/tok_l_xml.c
+++ lib/tok/tok_l_xml.c
@@ -2,10 +2,11 @@
 #include <runexpat.h>
 #include <tok.h>
 
+const char *curr_ref;
+
 void
 tok_l_sH(void *userData, const char *name, const char **atts)
 {
-#if 0
   Trun *r = userData;
   
   if (!strcmp(name, "xcl"))
@@ -15,14 +16,30 @@ tok_l_sH(void *userData, const char *name, const char **atts)
       const char *sig = findAttr(atts, "sig");
       if (!*sig)
 	sig = findAttr(atts, "exosig");
-      if (sig)
+      if (*sig)
 	fprintf(r->o, "l\t%s\t%s\t%s\t%s\n",
 		findAttr(atts, "ref"), sig, findAttr(atts, "exo_proj"), findAttr(atts, "exo_lang"));
+      else
+	curr_ref = strdup(findAttr(atts, "ref"));
     }
-#endif
+  else if (r->rs.in_xcl && !strcmp(name, "xff:f"))
+    {
+      const char *pos = findAttr(atts, "pos");
+      if (!strcmp(pos, "n"))
+	{
+	  fprintf(r->o, "l\t%s\t%s=%s\t\n",
+		  curr_ref, findAttr(atts, "form"), pos);
+	}
+    }
 }
 
 void
 tok_l_eH(void *userData, const char *name)
 {
+  Trun *r = userData;
+  if (r->rs.in_xcl && !strcmp(name, "l"))
+    {
+      free(curr_ref);
+      curr_ref = NULL;
+    }
 }
