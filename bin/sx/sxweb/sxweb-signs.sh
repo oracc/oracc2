b79d9373fd7e9206c5e2fe565160326571445100
Steve Tinney <stinney@upenn.edu>
2024-09-26 12:28:43 +0000

    sx fix osl forms/lex/sux selpages

--- bin/sx/sxweb/sxweb-signs.sh
+++ bin/sx/sxweb/sxweb-signs.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 project=$1
 asl=$2
+if [ "$project" = "" ]; then
+    project=osl
+fi
+if [ "$asl" = "" ]; then
+    asl=02xml/sl.xml
+fi
 libscripts=$ORACC_BUILDS/lib/scripts
 xsltproc -stringparam project $project -stringparam projesp "$ORACC_BUILDS/$project" $libscripts/sxweb-signs.xsl $asl
