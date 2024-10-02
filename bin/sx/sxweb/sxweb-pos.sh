b8852e7f485d2e3453f9007c65d00a8653125a1e
Steve Tinney <stinney@upenn.edu>
2024-10-02 02:47:51 +0000

    sxweb fix path in sxweb-pos.sh

--- bin/sx/sxweb/sxweb-pos.sh
+++ bin/sx/sxweb/sxweb-pos.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-for a in *.pos ; do
+for a in 00etc/*.pos ; do
     b=`basename $a .pos`
-    if [ -x 00bin/$a.sh ]; then
-	00bin/$a.sh
-    elif [ -x ${ORACC}/bin/$a.sh ]; then
-	$a.sh
+    if [ -x 00bin/$b.sh ]; then
+	00bin/$b.sh
+    elif [ -x ${ORACC}/bin/$b.sh ]; then
+	$b.sh
     fi
     xsltproc sxweb-page-o-signs.xsl $a >00lib/$b.xml
 done
