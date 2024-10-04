bbbaf9b7f5dd12375ef41a50fc470047c384106c
Stephen Tinney <stinney@upenn.edu>
2024-10-04 07:31:45 -0400

    xx/tis revised scripts for missing unicode

---
+++ bin/sx/sxweb/csl-need.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+#
+# Ensure that signlist/00etc/csl-need.txt is created with a list of
+# OIDs of signs that are not in Unicode
+#
+
---
+++ bin/xx/tis/crpsigns.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Generate a list of SL OIDs that is used in a set of XTF files.
+#
+p=$1
+if [ "$p" == "" ]; then
+    echo $0: must give project on command line. Stop.
+    exit 1
+fi
+s=${ORACC}/$p/01bld/lists/have-xtf.lst
+if [ ! -r $s ]; then
+    echo $0: no such file $s. Stop.
+    exit 1
+fi
+cat $s | tokx | grep ^g | cut -d. -f1 | cut -f2 | concord -nst | sort -k2nr
