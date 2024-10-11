dc858d9b1b9357acbf7db48742e9d157dd5c41ac
Steve Tinney <stinney@upenn.edu>
2024-10-11 11:34:19 +0000

    sx/dx/odo refine signlist build sequence

--- bin/dx/odo/odo-signlist.sh
+++ bin/dx/odo/odo-signlist.sh
@@ -2,10 +2,12 @@
 #
 # Perform build for a signlist project.
 #
-# odo always does an odo-update so there's no need to create the
-# indices etc. here.
+# odo always does an odo-update which calls sx-slix.sh so there's no
+# need to create the indices here if unless this is working within the
+# old o2 system.
 #
 echo $0 $*
+o2=$1
 projtype=`oraccopt . type`
 asl=""
 if [ "$projtype" = "corpus" ]; then
@@ -15,8 +17,16 @@ if [ "$projtype" = "corpus" ]; then
 	asl="01tmp/csl.asl"
     fi
 else
-    true # need to make stats for osl/pcsl
-    asl="static"
+    set 00lib/*.asl
+    if [ "$1" = "00lib/*.asl" ]; then
+	false
+    else
+	asl=$1
+    fi
+    if [ "$o2" = "o2" ]; then
+	sx-slix.sh `oraccopt` $asl
+    fi
+    # TODO: locate stats for osl/pcsl
 fi
 if [ "$asl" != "" ]; then
     sxweb.sh
