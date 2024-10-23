06a74d67bf885da4f42eccbe6073d0858797d791
Steve Tinney <stinney@upenn.edu>
2024-10-23 00:41:06 +0000

    fonts data and css work

--- lib/fonts/oraccttf.sh
+++ lib/fonts/oraccttf.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-showttfx $1 >ttf.txt
-grep '^ Glyph' ttf.txt | sed 's/^.*> //' | grep U+[1F]
-grep '<-' ttf.txt | cut -d'(' -f2 | cut -d')' -f1
-grep '\.[0-9]' ttf.txt | grep Base | cut -d'(' -f2 | cut -d')' -f1
+f=$1
+showttfx $f.ttf >$f-ttf.txt
+#grep '^ Glyph' ttf.txt | sed 's/^.*> //' | grep U+[1F]
+#grep '<-' ttf.txt | cut -d'(' -f2 | cut -d')' -f1
+#grep '\.[0-9]' ttf.txt | grep Base | cut -d'(' -f2 | cut -d')' -f1
+
+grep ^Glyph $f-ttf.txt | grep U+[1F] | sed 's/^.\+U+/U+/' >$f-data.txt
+grep ^Glyph $f-ttf.txt | grep _ | cut -d'"' -f2 >>$f-data.txt
+grep ^Glyph $f-ttf.txt | grep -F . | cut -d'"' -f2 | grep '^u[^n]' >>$f-data.txt
