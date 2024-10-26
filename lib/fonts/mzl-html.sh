76f7533935141084e986ee7d849ac9fbfbb1e390
stinney <stinney@upenn.edu>
2024-10-26 18:22:53 -0400

    lib/fonts first 100 OA signs realigned

--- lib/fonts/mzl-html.sh
+++ lib/fonts/mzl-html.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cut -f2 Oracc-Assyrian.data | sed 's/^ \+//' | paste mzl.lst - oracc-assyrian-utf8.lst >font.tab
+#cut -f2 Oracc-Assyrian.data | sed 's/^ \+//' | paste mzl.lst - oracc-assyrian-utf8.lst >font.tab
+paste mzl.lst oa-edit.lst >font.tab
 cat oa-head.html >oa.html
-rocox -n -R '<tr><td>%1</td><td>%2</td><td class="oa">%3</td></tr>' <font.tab >>oa.html
+perl -C -p -e 's#^(.*?)\t(.*?)\t(.*)$#<tr><td>$1</td><td>$2</td><td class="oa">$3</td></tr>#' <font.tab >>oa.html
+#rocox -n -R '<tr><td>%1</td><td>%2</td><td class="oa">%3</td></tr>' <font.tab >>oa.html
 cat oa-tail.html >>oa.html
