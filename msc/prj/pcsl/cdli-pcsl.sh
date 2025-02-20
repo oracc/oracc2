1d4e20c9311e73955a74ef704a5fc7337592e23f
Steve Tinney <stinney@upenn.edu>
2025-02-20 11:50:45 +0000

    cdli-pcsl.sh improve cat extraction

--- msc/prj/pcsl/cdli-pcsl.sh
+++ msc/prj/pcsl/cdli-pcsl.sh
@@ -15,13 +15,18 @@ cut -f1 pcsl-02-$cat >pcsl-cat.lst
 ./qpc.plx <cdli-20241011.atf | sort -u >cdli-atf-qpc.lst
 # List of texts not catalogued as Uruk IV/III but with lang=qpc in ATF
 comm -23 cdli-atf-qpc.lst pcsl-cat.lst >atf-qpc-only.lst
-# Union of cat list and atf-qpc list
-sort -u pcsl-cat.lst cdli-atf-qpc.lst >pcsl-all.lst
+# Remove atf-qpc-only that have period=Pre-Writing|Uruk V|ED I-II
+cut -f1,5 id_text-20241011.tsv | csvformat -tT | grep -f atf-qpc-only.lst >a-q-period.tsv
+grep -v 'Pre-Wri\|Uruk V\|ED I-II\|Ur III\|Babylonian\|P326290' a-q-period.tsv >a-q-bad-period.tsv
+# Union of cat list and atf-qpc bad period list
+sort -u pcsl-cat.lst a-q-bad-period.tsv >pcsl-all.lst
 # Get union entries from cat src
 sed 's/$/	/' pcsl-all.lst | grep -f - $cat >pcsl-03-$cat
 # Build Oracc format cat tsv starting with fields line from cat src
 head -1 $cat | sed 's/artifact_id/id_text/' >pcsl.tsv
-csvformat -tT <pcsl-03-$cat >>pcsl.tsv
+# A handful of CUSAS 21 texts mistakenly have period=ED IIIb so we fix
+# that on the fly
+csvformat -tT <pcsl-03-$cat | sed 's/ED IIIb/Uruk III/' >>pcsl.tsv
 # Get ATF corpus of all texts that ended up in pcsl cat
 cut -f1 pcsl.tsv | atfsplit.plx -cat -list - cdli-$date.atf >pcsl.atf
 # Package the core data
