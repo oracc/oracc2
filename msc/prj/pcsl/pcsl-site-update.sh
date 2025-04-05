75b972056cbb4e284d955bd5ebd06fb7e7d9c1a3
Steve Tinney <stinney@upenn.edu>
2025-04-05 13:55:37 +0000

    msc/prj pcsl build add newsl make

--- msc/prj/pcsl/pcsl-site-update.sh
+++ msc/prj/pcsl/pcsl-site-update.sh
@@ -2,3 +2,4 @@
 echo $0
 #(cd /home/stinney/orc/pcsl/pc25/w ; make)
 (cd /home/stinney/orc/pcsl ; pwd ; 00bin/update-pages.sh ; oracc build)
+(cd /home/stinney/orc/pcsl/newsl ; pwd ; make)
--- msc/prj/pcsl/pcsl-update-index.sh
+++ msc/prj/pcsl/pcsl-update-index.sh
@@ -1,3 +1,5 @@
 #!/bin/dash
-(cd /home/stinney/orc/pcsl ; sudo cp -a 00lib/pcsl.asl /home/oracc/pcsl/00lib)
+(cd /home/stinney/orc/pcsl ;
+ sudo cp -a 00lib/pcsl.asl /home/oracc/pcsl/00lib ;
+ sudo cp -a 00etc/im-cdli-gh.tab /home/oracc/pcsl/00etc)
 (cd /home/oracc/pcsl ; sudo /home/oracc/bin/orc /home/oracc/bin/sx-update.sh)
