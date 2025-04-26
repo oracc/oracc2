74d52ce18aaa80e1321e315063311aaabbdb2e32
Steve Tinney <stinney@upenn.edu>
2025-04-26 16:02:53 -0400

    tex fixes for pcsl/prop

--- bin/dx/odo/odo-catalog.sh
+++ bin/dx/odo/odo-catalog.sh
@@ -3,3 +3,10 @@ echo $0 $*
 o2-lst.sh
 lx-lists.sh
 o2-cat.sh
+if [ -r 01bld/cdlicat.xmd ]; then
+    project=`oraccopt`
+    mkdir -p 02pub/cat
+    echo qindex qcatx $project 01bld/cdlicat.xmd
+    qcatx -p $project -s <01bld/cdlicat.xmd
+    sudo chmod -R go+r 02pub/cat
+fi
