7d5bc88acb4bb37e2b13d1bd41db16a5c3b89ff8
Steve Tinney <stinney@upenn.edu>
2025-09-04 10:58:13 +0000

    various tweaks for build-oracc compile

--- bin/dx/odo/odo-sudo.sh
+++ bin/dx/odo/odo-sudo.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-me=`whoami`
-if [ $me != "root" ]; then
-    groups=`groups | grep -c sudo`
-    if [ "$groups" = "1" ]; then
-	echo sudo
+if [ "$ORACC_MODE" = "multi" ]; then
+    me=`whoami`
+    if [ $me != "root" ]; then
+	groups=`groups | grep -c sudo`
+	if [ "$groups" = "1" ]; then
+	    echo sudo
+	fi
     fi
 fi
