1a71a6e5b506b3ed5c6e1c1574d246d5ad72d15d
Steve Tinney <stinney@upenn.edu>
2025-09-05 17:14:22 -0400

    bin/xx cfy subs basically functional but need work

--- bin/xx/cun/cfy_subs.c
+++ bin/xx/cun/cfy_subs.c
@@ -48,10 +48,55 @@ sub_match(Cfy *c, Elt **epp, int i, Subspec *hsp)
   return msp;
 }
 
+/* For ELT_G types, import class, oid, xid from original data to replacement sp->r */
+static Elt **
+sub_r_fix(Cfy *c, Eltline *elp, int i, Subspec *sp)
+{
+  Elt **nr = memo_new_array(c->m_subspec, sp->r_len);
+  Elt *o_g = elp->epp[i]; /* matches always start with ELT_G */
+  memcpy(nr, sp->r, sizeof(Elt*) * sp->r_len);
+  int j;
+  for (j = 0; j < sp->r_len; ++j)
+    {
+      /* look for a match to k in elp->epp starting at elt j */
+      if (sp->r[j]->key)
+	{
+	  int k = 0;
+	  Elt *match = NULL;
+	  while (k < sp->l_len)
+	    {
+	      if (elp->epp[i+k]->key)
+		{
+		  if (!strcmp(elp->epp[i+k]->key, sp->r[j]->key))
+		    {
+		      match = elp->epp[i+k];
+		      break;
+		    }
+		}
+	      ++k;
+	    }
+	  /* if found, use the info from that match */
+	  if (match)
+	    *nr[j] = *match;
+	  else
+	    {
+	      /* state inheritance has to be by individual members here
+		 oid, data, key all need to be set when the sub rhs element is created
+	       */
+	      nr[j]->c = o_g->c;
+	      nr[j]->btype = o_g->btype;
+	      nr[j]->xid = o_g->xid;
+	    }
+	}
+    }
+  return nr;
+}
+
 static Elt **
 sub_replace(Cfy *c, Eltline *elp, int i, Subspec *sp, int *ip)
 {
   Elt**dest = elp->epp;
+  Elt **new_r = sub_r_fix(c, elp, i, sp);
   if (sp->r_len > sp->l_len)
     {
       int nlen = elp->len + (sp->r_len - sp->l_len) + 1;
@@ -60,20 +105,20 @@ sub_replace(Cfy *c, Eltline *elp, int i, Subspec *sp, int *ip)
 						   segment */
       dest = memo_new_array(c->m_elt, nlen);
       memcpy(dest, elp->epp, i * sizeof(Elt*));
-      memcpy(dest+i, sp->r, sp->r_len * sizeof(Elt*));
+      memcpy(dest+i, new_r, sp->r_len * sizeof(Elt*));
       memcpy(dest+i+sp->r_len, elp->epp+i+sp->l_len, last_seg * sizeof(Elt*));
       *ip = nlen;      
     }
   else if (sp->r_len == sp->l_len)
     {
-      memcpy(dest+i, sp->r, sp->r_len * sizeof(Elt*));
+      memcpy(dest+i, new_r, sp->r_len * sizeof(Elt*));
     }
   else
     {
       int last_seg = elp->len - (i+sp->l_len); 	/* Elt* after the end
 						   of the replaced
 						   segment */
-      memcpy(dest+i, sp->r, sp->r_len * sizeof(Elt*));
+      memcpy(dest+i, new_r, sp->r_len * sizeof(Elt*));
       memmove(dest+i+sp->r_len, elp->epp+i+sp->l_len, last_seg * sizeof(Elt*));
       *ip = i + sp->r_len + last_seg;
       dest[*ip] = NULL;
