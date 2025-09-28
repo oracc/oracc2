859cc74c625cba31633faec5c9a96ab1529eee8b
Steve Tinney <stinney@upenn.edu>
2025-09-28 23:48:58 +0000

    xx/cun cfy demo script tweaks

--- bin/xx/cun/t/demo.sh
+++ bin/xx/cun/t/demo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-./demo.plx >demo.html
-demodir=${ORACC}/www/osl/cuneify/demo
+./demo.plx >index.html
+demodir=${ORACC}/www/osl/OraccCuneiformFonts/Cuneify/demo/
 $ORACC_sudo mkdir -p $demodir
 d=`pwd`
-$ORACC_sudo cp $d/demo.html $d/demo.css $d/t*.html ${ORACC}/www/osl/cuneify/demo/
+$ORACC_sudo cp $d/index.html $d/demo.css $d/t*.html $demodir
 $ORACC_sudo chmod -R o+r $demodir
