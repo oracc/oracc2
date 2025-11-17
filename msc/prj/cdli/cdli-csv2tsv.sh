d886f43650422540aa0dff6c8550a5451afd34a7
Steve Tinney <stinney@upenn.edu>
2025-11-17 12:20:53 +0000

    msc/prj/cdli cleanup CDLI cat install scripts

--- msc/prj/cdli/cdli-csv2tsv.sh
+++ msc/prj/cdli/cdli-csv2tsv.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# This script requires cdli.csv in 00src
+#
 # CDLI cat has sporadic control chars from other cat imports so
 # we have to be very careful about line-end remapping:
-tr '' ' ' <cdli.csv | csvformat -T -M '' | tr '\n\r' '     ' | tr '' '\n' >cdli.tsv
+(cd 00src ;
+tr '' ' ' <cdli.csv | csvformat -T -M '' | tr '\n\r' '     ' | tr '' '\n' >cdli.tsv )
