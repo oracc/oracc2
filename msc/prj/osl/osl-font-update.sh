3aed603076a4b3d93ad579e856a14dd481935c6b
Steve Tinney <stinney@upenn.edu>
2025-12-12 08:37:07 -0500

    msc/prj/osl streamline OSL build

--- msc/prj/osl/osl-font-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo $0
-if [ "$ORACC_MODE"="multi" ]; then
-   sudo=sudoi
-fi
-
-sudo ./osl-prepare-update.sh
-#sudo su -l ogsl /home/ogsl/osl/00bin/osl-font-update.sh
-sudo ./osl-sx-slix.sh
