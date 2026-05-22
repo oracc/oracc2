7d02de8ff1e50a32a15ea4f6f033a121cb013a77
Steve Tinney <stinney@upenn.edu>
2026-05-22 10:21:01 -0400

    lib/cbd fix bugs in bases of compounds and in compound parts which aren't simple CGP

--- lib/cbd/cbd_psus.c
+++ lib/cbd/cbd_psus.c
@@ -22,7 +22,48 @@ psu_index(Entry *part, Entry *psu)
   list_add(lp, psu);
 }
 
+static char *
+sanitize_cgp(const char *cfg)
+{
+  char buf[strlen(cfg)], *bp = buf;
+  const char *s = cfg, *x;
+  *bp = '\0';
+  while (*s)
+    {
+      while (' ' == *s)
+	++s;
+      if ('n' == s[0] && ' ' == s[1])
+	s += 2;
+      else if ((x = strstr(s, "//")))
+	{
+	  while (s < x)
+	    *bp++ = *s++;
+	  *bp = '\0';
+	  s = strchr(s, ']');
+	  while ('\'' != *s)
+	    *bp++ = *s++;
+	  *bp = '\0';
+	  while (*s && !isspace(*s))
+	    ++s;
+	}
+      else
+	{
+	  while (*s)
+	    *bp++ = *s++;
+	  *bp = '\0';
+	}
+    }
+  return strdup(buf);
+}
+
 /* validate @parts, linking each cgp to its Entry.
+ *
+ * Two things need better implementation:
+ *
+ * n in @parts, e.g., n ŋal[be]V/it--these are currently just ignored when checking
+ *
+ * //SENSE in @parts CGP; this is currently stripped out in checking but should use SENSE_GW
+ *
  */
 void
 cbd_psus(void)
@@ -35,12 +76,17 @@ cbd_psus(void)
       Hash *seen = hash_create(0);
       for (cp = list_first(p->cgps); cp; cp = list_next(p->cgps))
 	{
-	  Entry *ep = hash_find(pcbd->hentries, cp->tight);
+	  char *c = (char*)cp->tight, *m = NULL;
+	  if (('n' == c[0] && ' ' == c[1])
+	      || strstr(c, " n ")
+	      || strstr(c, "//"))
+	    c = m = sanitize_cgp(c);
+	  Entry *ep = hash_find(pcbd->hentries, (uccp)c);
 	  if (ep)
 	    cp->owner = ep;
 	  else
 	    {
-	      if ((ep = xcbd_find(ep,cp->tight,pcbd)))
+	      if ((ep = xcbd_find(ep,(uccp)c,pcbd)))
 		cp->owner = ep;
 	      else
 		{
@@ -50,15 +96,17 @@ cbd_psus(void)
 				      should be done */
 		}
 	    }
-	  if (cp->owner && !hash_find(seen, cp->owner->cgp->tight))
+	  if (cp->owner && !hash_find(seen, (uccp)c))
 	    {
 	      psu_index(cp->owner, p->owner); /* index the current part
 						 as occurring in the
 						 current PSU */
 	      /* if a part occurs more than once in a PSU only index
 		 it once to avoid faffing with uniqing later */
-	      hash_add(seen, cp->owner->cgp->tight, "");
+	      hash_add(seen, (uccp)c, "");
 	    }
+	  if (m)
+	    free(m);
 	}
       hash_free(seen, NULL);  
     }
