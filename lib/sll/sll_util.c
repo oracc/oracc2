88939ec7659ec9612a4265805b2e6d89c10ca09e
Steve Tinney <stinney@upenn.edu>
2026-05-25 13:35:57 -0400

    lib/sll fix sll_snames_of to understand new use of '_' in multiword sigs

--- lib/sll/sll_util.c
+++ lib/sll/sll_util.c
@@ -332,11 +332,11 @@ sll_snames_of(unsigned const char *oids)
     {
       const char *xsave = NULL;
       xoid = x;      
-      while (*x && !(strchr(".&%*:+- ", *x)))
+      while (*x && !(strchr(".&%*:+-_", *x)))
 	++x;
       if (*x)
 	{
-	  xsave = (*x == ' ' ? " " : ".");
+	  xsave = (*x == '_' ? " " : ".");
 	  *x++ = '\0';
 	}
       list_add(l,(void*)sll_lookup(xoid));
