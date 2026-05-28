680845ecb30d526752cfea29b761657181ce1ec9
Steve Tinney <stinney@upenn.edu>
2026-05-28 16:24:35 -0400

    lib/cbd / bin/gx gx -C NEW OLD now lists +/- needed for NEW

--- lib/cbd/cbd_cmp.c
+++ lib/cbd/cbd_cmp.c
@@ -29,19 +29,27 @@ cmp_s_only_b(List *ls, Hash *hseen)
 }
 
 static void
-cbd_cmp_list_e(List *lp, const char *fn)
+cbd_cmp_need_plus_e(List *lp, const char *fn)
 {
   const char *cgp;
   for (cgp = list_first(lp); cgp; cgp = list_next(lp))
-    fprintf(stderr, "entry %s only in glossary %s\n", cgp, fn);
+    fprintf(stderr, "+%s\n", cgp);
 }
 
 static void
-cbd_cmp_list_s(List *lp, const char *fn)
+cbd_cmp_need_minus_e(List *lp, const char *fn)
 {
   const char *cgp;
   for (cgp = list_first(lp); cgp; cgp = list_next(lp))
-    fprintf(stderr, "sense %s only in glossary %s\n", cgp, fn);
+    fprintf(stderr, "-%s\n", cgp);
+}
+
+static void
+cbd_cmp_need_s(List *lp, char need, const char *fn)
+{
+  const char *cgp;
+  for (cgp = list_first(lp); cgp; cgp = list_next(lp))
+    fprintf(stderr, "%c%s\n", need, cgp);
 }
 
 static int
@@ -84,13 +92,13 @@ cbd_cmp_senses(Entry *ea, Entry *eb)
   List *l_only_b = cmp_s_only_b(eb->senses, h_seen_s);
   if (list_len(l_only_a))
     {
-      cbd_cmp_list_s(l_only_a, ea->owner->file);
+      cbd_cmp_need_s(l_only_a, '+', ea->owner->file);
       ++ret;
     }
 
   if (list_len(l_only_b))
     {
-      cbd_cmp_list_s(l_only_b, eb->owner->file);
+      cbd_cmp_need_s(l_only_b, '-', eb->owner->file);
       ++ret;
     }  
 
@@ -103,6 +111,9 @@ cbd_cmp_entry(Entry *tep, Entry *fep)
   return cbd_cmp_senses(tep, fep);
 }
 
+/* This routine compares @entry/@sense to inform how the new glossary,
+   cpa, should be marked up with +/- to suppress all differences with
+   the old glossary, cpb */
 int
 cbd_cmp(Cbd *cpa, Cbd *cpb)
 {
@@ -111,42 +122,42 @@ cbd_cmp(Cbd *cpa, Cbd *cpb)
   List *l_only_a = list_create(LIST_SINGLE);
   Hash *h_seen_b = hash_create(128);
   
-  Entry *fe;
-  for (fe = list_first(cpa->entries); fe; fe = list_next(cpa->entries))
+  Entry *ea;
+  for (ea = list_first(cpa->entries); ea; ea = list_next(cpa->entries))
     {
-      Entry *ep = hash_find(cpb->hentries, fe->cgp->tight);
-      if (ep)
+      Entry *eb = hash_find(cpb->hentries, ea->cgp->tight);
+      if (eb)
 	{
-	  ret += cbd_cmp_entry(ep, fe);
-	  hash_add(h_seen_b, fe->cgp->tight, fe);
+	  ret += cbd_cmp_entry(ea, eb);
+	  hash_add(h_seen_b, eb->cgp->tight, eb);
 	}
       else
 	{
-	  if (fe->ed)
+	  if (ea->ed)
 	    {
-	      switch (fe->ed->type)
+	      switch (ea->ed->type)
 		{
 		case ADD_E:
 		  break;
 		default:
-		  list_add(l_only_a, (void*)fe->cgp->tight);
+		  list_add(l_only_a, (void*)ea->cgp->tight);
 		  break;
 		}
 	    }
 	  else
-	    list_add(l_only_a, (void*)fe->cgp->tight);
+	    list_add(l_only_a, (void*)ea->cgp->tight);
 	}
     }
   
   List *l_only_b = cmp_only_b(cpb, h_seen_b);
   if (list_len(l_only_a))
     {
-      cbd_cmp_list_e(l_only_a, cpa->file);
+      cbd_cmp_need_plus_e(l_only_a, cpa->file);
       ++ret;
     }
   if (list_len(l_only_b))
     {
-      cbd_cmp_list_e(l_only_b, cpb->file);
+      cbd_cmp_need_minus_e(l_only_b, cpa->file);
       ++ret;
     }
 
